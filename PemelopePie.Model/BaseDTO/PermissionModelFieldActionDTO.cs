aa59edf533cffb6cf0e27a12d4f5bd7bba7dde44
Wan <wmadzha11@hotmail.com>
2020-02-07 16:38:32 +0800

    Add Models

---
+++ PemelopePie.Model/BaseDTO/PermissionModelFieldActionDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PemelopePie.Model.BaseDTO
+{
+    public class PermissionModelFieldActionDTO
+    {
+        public Guid PermissionModelPropertiesActionId { get; set; }
+        public string ActionType { get; set; }
+        public string FieldName { get; set; }
+        public string ModelName { get; set; }
+        public string GrantType { get; set; }
+        public Guid? EntityFieldsId { get; set; }
+        public Guid? EntitiesId { get; set; }
+    }
+}
---
+++ PemelopePie.Model/BaseDTO/ProjectDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PemelopePie.Model.BaseDTO
+{
+    public class ProjectDTO
+    {
+        public Guid ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+    }
+}
